224419164581eae20f41a684d528d088660ee77a
Benjamin Tovar <btovar@nd.edu>
2023-02-15 09:28:45 -0500

    cleanup recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,14 +3,7 @@
 DISABLED_SYS=$(echo --without-system-{parrot,prune,umbrella,weaver})
 DISABLED_LIB=$(echo --with-{readline,fuse,perl}-path\ no)
 
-
-if [[ "$PY3K" == 1 ]]; then
-    PYTHON_OPT="--with-python3-path"
-else
-    PYTHON_OPT="--with-python2-path"
-fi
-
-./configure --debug --prefix "${PREFIX}" --with-base-dir "${PREFIX}" ${PYTHON_OPT} "${PREFIX}" ${DISABLED_LIB} ${DISABLED_SYS}
+./configure --debug --prefix "${PREFIX}" --with-base-dir "${PREFIX}" ${DISABLED_LIB} ${DISABLED_SYS}
 
 echo ==config.mk==
 cat config.mk
